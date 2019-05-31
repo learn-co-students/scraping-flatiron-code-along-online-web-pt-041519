8d856bd7d50863095b2502c76f402aac6658cd3a
github username <github email address>
2019-05-30 22:14:06 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description #setter/getter reader/writer
 
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all #Class #setter/writer
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
